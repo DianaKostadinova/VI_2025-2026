e75b62bf6c437c9fb7fea1a92e5101215365b4d6
DianaKostadinova <dkostadinova112@gmail.com>
2026-05-05 20:03:33 +0200

    lab5

---
+++ lab5/1.py
@@ -0,0 +1,40 @@
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+from submission_script import *
+from dataset_script import dataset
+
+from sklearn.naive_bayes import GaussianNB
+
+if __name__ == '__main__':
+    record = list(map(float, input().split()))
+
+    class_0 = [row for row in dataset if row[-1] == '0']
+    class_1 = [row for row in dataset if row[-1] == '1']
+
+    split_0 = int(len(class_0) * 0.85)
+    split_1 = int(len(class_1) * 0.85)
+
+    train = class_0[:split_0] + class_1[:split_1]
+    test = class_0[split_0:] + class_1[split_1:]
+
+    train_X = [list(map(float, row[:-1])) for row in train]
+    train_Y = [row[-1] for row in train]
+
+    test_X = [list(map(float, row[:-1])) for row in test]
+    test_Y = [row[-1] for row in test]
+
+    classifier = GaussianNB()
+    classifier.fit(train_X, train_Y)
+
+    accuracy = classifier.score(test_X, test_Y)
+    pred = classifier.predict([record])
+    prob = classifier.predict_proba([record])
+
+    print(accuracy)
+    print(pred[0])
+    print(prob)
+
+    submit_train_data(train_X, train_Y)
+    submit_test_data(test_X, test_Y)
+    submit_classifier(classifier)
---
+++ lab5/submission_script.py
@@ -0,0 +1,4 @@
+def submit_train_data(X, Y): pass
+def submit_test_data(X, Y): pass
+def submit_classifier(c): pass
+def submit_encoder(e): pass
